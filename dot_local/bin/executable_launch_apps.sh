caf6b12f71d1e6cd6ae1e34e4799f10ac44914cc
Samuel <samuel@example.com>
2026-04-13 15:17:36 -0600

    fix start up apps

--- dot_local/bin/executable_launch_apps.sh
+++ dot_local/bin/executable_launch_apps.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Launch Zen Personal
-env MOZ_APP_REMOTINGNAME=zen-personal zen-browser --profile "$HOME/.config/zen/zen.personal" --new-instance &
+env MOZ_APP_REMOTINGNAME=zen-personal zen-browser -P "personal" --new-instance &
+
+# Give it a second to start
+sleep 2
 
 # Launch Zen Utility
-env MOZ_APP_REMOTINGNAME=zen-utility zen-browser --profile "$HOME/.config/zen/zen.utility" --new-instance &
+env MOZ_APP_REMOTINGNAME=zen-utility zen-browser -P "utility" --new-instance &
+
+# Give it a second to start
+sleep 2
 
 # Launch Quod Libet
 flatpak run io.github.quodlibet.QuodLibet &
