fb8daca040a3b6f58960312ae77e8922576e9643
Samuel <samuel@example.com>
2026-05-06 14:56:56 -0600

    update

--- dot_local/bin/executable_launch_apps.sh
+++ dot_local/bin/executable_launch_apps.sh
@@ -6,8 +6,8 @@ env MOZ_APP_REMOTINGNAME=zen-personal zen-browser -P "personal" --new-instance &
 # Give it a second to start
 sleep 2
 
-# Launch Helium
-helium &
+# Launch Zen Utility
+env MOZ_APP_REMOTINGNAME=zen-utility zen-browser --profile /var/home/samuel/.config/zen/zen.utility &
 
 # Give it a second to start
 sleep 2
