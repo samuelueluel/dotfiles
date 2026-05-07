deb735cca052e13e94239f950c10d6d806d5bf84
Samuel <samuel@example.com>
2026-05-07 12:06:09 -0600

    update

--- dot_local/bin/executable_launch_apps.sh
+++ dot_local/bin/executable_launch_apps.sh
@@ -7,7 +7,7 @@ env MOZ_APP_REMOTINGNAME=zen-personal zen-browser -P "personal" --new-instance &
 sleep 2
 
 # Launch Zen Utility
-env MOZ_APP_REMOTINGNAME=zen-utility zen-browser --profile /var/home/samuel/.config/zen/zen.utility &
+env MOZ_APP_REMOTINGNAME=zen-utility zen-browser -P "utility" --new-instance &
 
 # Give it a second to start
 sleep 2
