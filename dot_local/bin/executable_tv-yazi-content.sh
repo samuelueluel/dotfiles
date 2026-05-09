5764017e08a8408d70221a4e71b2736b366a34b1
Samuel <samuel@example.com>
2026-05-09 15:31:32 -0600

    update

--- dot_local/bin/executable_tv-yazi-content.sh
+++ dot_local/bin/executable_tv-yazi-content.sh
@@ -7,14 +7,13 @@ read -r query
 
 selected=$(
     $RG -l "$query" "$PWD" |
-    sed "s|$PWD/||" |
     tv --source-command="cat" \
-       --preview-command="$RG --color=always -C2 '$query' \"$PWD\"/{}" \
+       --preview-command="$RG --color=always -C2 '$query' \"{}\"" \
        --preview-size=60 \
-       --input-header="Results: $query"
+       --input-header="Results: $query" \
+       --layout=portrait
 )
 
 [ -z "$selected" ] && exit 0
 
-TARGET="$PWD/$selected"
-niri msg action spawn -- kitty -e yazi "$TARGET"
+niri msg action spawn -- kitty -e yazi "$selected"
