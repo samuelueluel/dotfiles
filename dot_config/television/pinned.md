2969ab1eda547abf7e5084bff08742161f918d05
Samuel <samuel@example.com>
2026-05-10 18:47:16 -0600

    update

---
+++ dot_config/television/pinned.md
@@ -0,0 +1,11 @@
+# Favorite Directories
+
+- /var/home/samuel/Dropbox/
+- /var/home/samuel/Documents/
+- /var/home/samuel/dotfiles/
+- /var/home/samuel/samuel-niri/
+- /var/home/samuel/system_config_git/
+- /var/home/samuel/.config/
+- /var/home/samuel/.config/television/
+- /var/home/samuel/.config/rmpc/
+- /var/home/samuel/.config/waybar/
--- dot_local/bin/executable_tv-yazi-content.sh
+++ dot_local/bin/executable_tv-yazi-content.sh
@@ -7,8 +7,22 @@ read -r query
 
 selected=$(
     $RG -l "$query" "$PWD" |
+    awk '{
+      match($0, /.*\//);
+      dir = substr($0, 1, RLENGTH);
+      file = substr($0, RLENGTH+1);
+      prefix = "/var/home/samuel/";
+      if (index(dir, prefix) == 1) {
+        rest_dir = substr(dir, length(prefix) + 1);
+        printf "\033[38;2;208;167;175m%s\033[36m%s\033[0m%s\n", prefix, rest_dir, file;
+      } else {
+        printf "\033[36m%s\033[0m%s\n", dir, file;
+      }
+    }' |
     tv --source-command="cat" \
-       --preview-command="$RG --color=always -C2 '$query' \"{}\"" \
+       --ansi \
+       --source-output="{strip_ansi}" \
+       --preview-command="$RG --color=always -C2 '$query' \"{strip_ansi}\"" \
        --preview-size=60 \
        --input-header="Results: $query" \
        --layout=portrait
